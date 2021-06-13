3e2ebef4e2833f266f23876709155cc694edf593
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-13 17:51:52 +0900

    update: lesson9
    https://gitmoji.dev/

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const buttonVariants = {
+  // visible: {
+  //   x: [0, -20, 20, -20, 20, 0],
+  //   transition: { delay: 2 },
+  // },
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1],
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
@@ -12,13 +24,7 @@ const Home = () => {
     >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: '0px 0px 8px rgb(255,255,255)',
-            boxShadow: '0px 0px 8px rgb(255,255,255)',
-          }}
-        >
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
