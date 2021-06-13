747afdc910aea4751e63fa08efb00a21b43a01d8
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-06-13 15:46:48 +0900

    remove: アニメーション削除
    https://gitmoji.dev/

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -4,14 +4,12 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <motion.div className="home container" animate={{ opacity: 0.2, marginTop: 200 }}>
-      <h2 animate={{ rotateZ: 180, fontSize: 50, color: '#ff2994', x: 100, y: -100 }}>
-        Welcome to Pizza Joint
-      </h2>
+    <div className="home container">
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{ scale: 1.5 }}>Create Your Pizza</motion.button>
+        <button>Create Your Pizza</button>
       </Link>
-    </motion.div>
+    </div>
   );
 };
 
